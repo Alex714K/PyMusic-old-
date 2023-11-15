2e51abc404864b2c5ec914d95ff8a500f9293dc2
Alex714K <klimentenko.ad@gmail.com>
2023-11-15 20:19:41 +0300

    0.3.1

--- music2.py
+++ music2.py
@@ -1,8 +1,17 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets
+from mydesign import Ui_MainWindow  # импорт нашего сгенерированного файла
 import sys
 
-app = QtWidgets.QApplication([])
-win = uic.loadUi("mydesign.ui")  # расположение вашего файла .ui
 
-win.show()
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.show()
+
 sys.exit(app.exec())
